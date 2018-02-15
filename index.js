d9e0d18dc4c48787a8c3977b3d749da63a4f4e02
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-02-14 22:43:11 -0800

    add All Moods nav

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ axios.get(`${path}/users`)
 //     console.log('ERROR!', err);
 //   })
 
+let reportsData = [];
 let reports;
 let latestMood;
 let latestTimeOfDay;
@@ -49,8 +50,13 @@ let latestFoodId;
 let timestamp;
 
 let formData = {
+  usersId: "",
   firstName: "",
   lastName: "",
+  timeOfDay: "",
+  mood: "",
+  toysId: "",
+  foodsId: "",
 }
 
 // GET all report
@@ -58,17 +64,16 @@ axios.get(`${path}/reports`)
   .then(res => {
     // let reports = res.data
     reports = res.data
-    let last = reports.length-1;
+    let last = reports[reports.length-1];
     // console.log(res)
     console.log('Reports!', reports)
-    latestMood = reports[last].mood;
-    latestTimeOfDay = reports[last].time_of_day;
-    latestUserId = reports[last].users_id;
-    latestToyId = reports[last].toys_id;
-    latestFoodId = reports[last].foods_id;
-    const latestTimestampRaw = reports[last].created_at;
+    latestMood = last.mood;
+    latestTimeOfDay = last.time_of_day;
+    latestUserId = last.users_id;
+    latestToyId = last.toys_id;
+    latestFoodId = last.foods_id;
+    const latestTimestampRaw = last.created_at;
     latestTimestamp = latestTimestampRaw.slice(0, 10);
-    // console.log('stuff', latestMood, latestTimeOfDay, latestUserId, latestToyId, latestFoodId, latestTimestamp)
 
     // update average moods
     updateAverageMood(reports)
@@ -77,12 +82,7 @@ axios.get(`${path}/reports`)
 
     // list all reports
     reports.forEach(report => {
-      // mood = report.mood;
-      // timeOfDay = report.time_of_day;
-      // userId = report.users_id;
-      // toyId = report.toys_id;
-      // foodId = report.foods_id;
-      // console.log('stuff', mood, timeOfDay, userId, toyId, foodId)
+      reportsData.push(report);
 
       let reportElement = document.createElement('div')
       // returnDetailData(report.user_id, ...)
@@ -97,11 +97,14 @@ axios.get(`${path}/reports`)
                                   Food: ${report.foods_id}
                                   `
       document.querySelector('#userList').appendChild(reportElement)
+
     })
   })
   .catch(err => {
     console.log('ERROR!', err);
   })
+
+console.log('RERERE', reportsData)
 
 
 // POST new user
@@ -146,37 +149,38 @@ axios.get(`${path}/reports`)
 //   return Promise.all(promiseArr);
 // }
 
-// TOYS
-
-
-
-
-// FOODS
-
 
 
 //***EVENT LISTENERS***
 // HOME nav
-document.querySelector('#home').addEventListener('click', homeNav)
-document.querySelector('#home').addEventListener('touchstart', homeNav)
+document.querySelector('#home').addEventListener('click', homeNav);
+document.querySelector('#home').addEventListener('touchstart', homeNav);
 
-// NEW ENTRY nav
-document.querySelector('#new-entry').addEventListener('click', newEntryNav)
-document.querySelector('#new-entry').addEventListener('touchstart', newEntryNav)
+// NEW MOOD ENTRY nav
+document.querySelector('#new-entry').addEventListener('click', newEntryNav);
+document.querySelector('#new-entry').addEventListener('touchstart', newEntryNav);
+
+//ALL MOODS nav
+document.querySelector('#all-moods').addEventListener('click', allMoodsNav);
+document.querySelector('#all-moods').addEventListener('touchstart', allMoodsNav);
+
+// DROPDOWN filter
+document.querySelector('.dropdown-menu').addEventListener('click', dropdownClick);
+document.querySelector('.dropdown-menu').addEventListener('touchstart', dropdownClick);
 
 // NEW ENTRY button
-document.querySelector('#reportNewMood').addEventListener('click', newEntryNav)
-document.querySelector('#reportNewMood').addEventListener('touchstart', newEntryNav)
+document.querySelector('#reportNewMood').addEventListener('click', newEntryNav);
+document.querySelector('#reportNewMood').addEventListener('touchstart', newEntryNav);
 
 // Selecting Options
-document.querySelector('.time-options').addEventListener('click', selectOptionItem)
-document.querySelector('.time-options').addEventListener('touchstart', selectOptionItem)
-document.querySelector('.mood-options').addEventListener('click', selectOptionItem)
-document.querySelector('.mood-options').addEventListener('touchstart', selectOptionItem)
-document.querySelector('.toy-options').addEventListener('click', selectOptionItem)
-document.querySelector('.toy-options').addEventListener('touchstart', selectOptionItem)
-document.querySelector('.food-options').addEventListener('click', selectOptionItem)
-document.querySelector('.food-options').addEventListener('touchstart', selectOptionItem)
+document.querySelector('.time-options').addEventListener('click', selectOptionItem);
+document.querySelector('.time-options').addEventListener('touchstart', selectOptionItem);
+document.querySelector('.mood-options').addEventListener('click', selectOptionItem);
+document.querySelector('.mood-options').addEventListener('touchstart', selectOptionItem);
+document.querySelector('.toy-options').addEventListener('click', selectOptionItem);
+document.querySelector('.toy-options').addEventListener('touchstart', selectOptionItem);
+document.querySelector('.food-options').addEventListener('click', selectOptionItem);
+document.querySelector('.food-options').addEventListener('touchstart', selectOptionItem);
 
 
 // SUBMIT MOOD button
@@ -204,11 +208,24 @@ function newEntryNav(event) {
   // event.preventDefault();
   document.querySelector('.home-container').style.display = "none";
   document.querySelector('.report-container').style.display = "block";
+  document.querySelector('.page-title').textContent = "Seen Mossy lately? Report her latest mood below.";
+  document.querySelector('.view-all').style.display = "none";
+  document.querySelector('.submit-new').style.display = "block";
 }
 
-function updateAverageMood(event, reports) {
-    console.log('???', reports)
+function allMoodsNav(event) {
+  // event.preventDefault();
+  document.querySelector('.home-container').style.display = "none";
+  document.querySelector('.report-container').style.display = "block";
+  document.querySelector('.page-title').textContent = "";
+  document.querySelector('.view-all').style.display = "block";
+  document.querySelector('.submit-new').style.display = "none";
+}
+
+function updateAverageMood(event) {
+    console.log('???', reportsData)
     // document.querySelector('.lastDate').textContent = `${latestTimestamp}`;
+
 }
 
 function updateLatestMood(event) {
@@ -218,6 +235,16 @@ function updateLatestMood(event) {
   document.querySelector('.lastMood').textContent = `Mood: ${latestMood}`;
   document.querySelector('.lastToy').textContent = `Toy: ${latestToyId}`;
   document.querySelector('.lastFood').textContent = `Food: ${latestFoodId}`;
+}
+
+function dropdownClick(event) {
+  console.log('what was clicked', event.target)
+  let clickId = event.target.id
+  console.log(clickId)
+
+  document.querySelector('.selection-title').textContent = `${clickId}`;
+
+
 }
 
 function selectOptionItem(event) {
@@ -240,6 +267,7 @@ function selectOptionItem(event) {
     }
   })
   workingTarget.classList.toggle('selected');
+
 }
 
 function submitReport(event) {
@@ -255,4 +283,6 @@ function submitReport(event) {
   // reset view to Homepage
   document.querySelector('.home-container').style.display = "";
   document.querySelector('.report-container').style.display = "none";
+
+  updateLatestMood()
 }
